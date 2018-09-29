ca1dc77985fb8b97157df6084354d69e7caf5246
leixu <349106576@qq.com>
2018-09-29 19:04:39 +0800

    init

--- ConfigManager/config.py
+++ ConfigManager/config.py
@@ -8,6 +8,7 @@
  * IDE PyCharm
 '''
 import configparser
+import time
 
 
 class BaseConfig(object):
@@ -17,6 +18,7 @@ class BaseConfig(object):
 class Config(object):
     def __init__(self, static_path=None, url=None):
         self.config = None
+        self.p = None
         if static_path:
             self.reload_config_file(static_path)
         elif url:
@@ -25,21 +27,31 @@ class Config(object):
     def reload_online(self, url):
         pass
 
+    def load_dict(self, _dict):
+        return
+
     def reload_config_file(self, path):
         self.config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
         self.config.read(path, encoding="utf8")
+
+    def load_str(self, _str):
+        self.config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+        self.config.read_string(_str)
 
     def style_json(self):
         pass
 
     def style_class(self):
-        _o = BaseConfig()
-        for section in self.config.sections():
-            setattr(_o, section, BaseConfig())
-            for key in self.config.options(section):
-                sub_config = getattr(_o, section)
-                setattr(sub_config, key, self.config.get(section, key))
-        return _o
+        if isinstance(self.config, configparser.ConfigParser):
+            _o = BaseConfig()
+            for section in self.config.sections():
+                setattr(_o, section, BaseConfig())
+                for key in self.config.options(section):
+                    sub_config = getattr(_o, section)
+                    setattr(sub_config, key, self.config.get(section, key))
+            return _o
+        else:
+            pass
 
     def style_configparser(self):
         return self.config
